929bc8072c94dbd7429d8a734fed24be70dfb814
wujiale-spec <1284698788@qq.com>
2020-10-10 17:26:20 +0800

    first commit

---
+++ dom.js
@@ -0,0 +1,28 @@
+window.dom = {
+    find(selector,scope){
+        return  (scope || document).querySelectorAll(selector)
+    },
+    style(node,name,value){
+        if(arguments.length===3){
+            //dom.style(test,'color','blue')
+            node.style[name]=value
+        }else if(arguments.length===2){
+            if(typeof name==='string'){
+                //dom.style(test,'color')
+                return node.style[name]
+            }else if(name instanceof Object){
+                //dom.style(test,{color:'red'})
+                const object=name
+                for(let key in object){
+                    node.style[key]=object[key]
+        }
+            }
+        }
+    },
+    each(nodeList,fn){
+        for(let i=0;i<nodeList.length;i++){
+            fn.call(null,nodeList[i])
+        }
+    }
+}
+
